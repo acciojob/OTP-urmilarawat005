7d0cb8d2b02902cb693ee527cfb185012483fb83
urmilarawat005 <u4urmilarawat@gmail.com>
2024-12-26 21:31:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,24 +1,14 @@
- // Get all OTP input elements
-    const otpInputs = Array.from(document.querySelectorAll('.code'));
+let codes = document.querySelectorAll(".code");
+            codes[0].focus();
 
-    // Focus on the next input after typing a character
-    otpInputs.forEach((input, index) => {
-      input.addEventListener('input', () => {
-        if (input.value) {
-          // Focus next input if there is input
-          if (index < otpInputs.length - 1) {
-            otpInputs[index + 1].focus();
-          }
-        }
-      });
-
-      // Handle backspace on keyup event
-      input.addEventListener('keyup', (e) => {
-        if (e.key === 'Backspace' && !input.value) {
-          // Focus previous input if current input is empty
-          if (index > 0) {
-            otpInputs[index - 1].focus();
-          }
-        }
-      });
-    });
+            codes.forEach((code, idx) =>{
+                code.addEventListener("keydown" , (e) => {
+                    if(e.key >= 0 && e.key <=9){
+                        codes[idx].value = "";
+                        setTimeout(() => codes[idx+1].focus(),10);
+                    }else if (e.key === "Backspace"){
+                        setTimeout(() => codes[idx-1].focus(),10);
+                        }
+                    }
+                )
+                })
